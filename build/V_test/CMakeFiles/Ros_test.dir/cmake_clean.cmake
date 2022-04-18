b4fc94d372c6f269b3345562377f939c31840a06
g <cxangyue@gmail.com>
2022-04-18 17:54:39 +0800

    git config user.name "Glennine" git config user.email "cxangyue@gmail.com"

---
+++ build/V_test/CMakeFiles/Ros_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ros_test.dir/main.cpp.o"
+  "Ros_test"
+  "Ros_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ros_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
