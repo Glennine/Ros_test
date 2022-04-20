fe7dce6f423c6aa93c6044550e5bf2b93406179e
g <cxangyue@gmail.com>
2022-04-20 12:12:42 +0800

    git config user.name "Glennine" git config user.email "cxangyue@gmail.com"

---
+++ build/V_test/CMakeFiles/V_test_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/g/CLionProjects/Ros_test/devel/lib/V_test/V_test_node"
+  "/home/g/CLionProjects/Ros_test/devel/lib/V_test/V_test_node.pdb"
+  "CMakeFiles/V_test_node.dir/src/test_node.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/V_test_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/cmake-build-debug/V_test/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/g/CLionProjects/Ros_test/src/V_test/src
+# Build directory: /home/g/CLionProjects/Ros_test/src/cmake-build-debug/V_test/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
